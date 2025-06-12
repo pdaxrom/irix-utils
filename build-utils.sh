272520652424ff52fde4bdc1b90387ee06f1a8df
pdaxrom <sashz@pdaxrom.org>
2025-06-12 06:41:12 +0000

    fixed mc build, ssh init

--- build-utils.sh
+++ build-utils.sh
@@ -419,7 +419,7 @@ if ! test -e mc.installed; then
     cd b
     cat ${TOPDIR}/caches/*.cache > mc.cache
 
-    ../configure --prefix=$INST_PREFIX --host=${CROSS_PREFIX} --libdir=$LIBDIR_PREFIX --with-screen=ncurses --with-x --with-ncurses-includes=${INST_PREFIX}/include --with-ncurses-libs=${LIBDIR_PREFIX} CPPFLAGS="-std=gnu99 -I${INST_PREFIX}/include" LDFLAGS="${COMPAT_IRIX_LIB} -L${LIBDIR_PREFIX} -Wl,-rpath-link,${LIBDIR_PREFIX}" --cache-file=mc.cache
+    ../configure --prefix=$INST_PREFIX --host=${CROSS_PREFIX} --libdir=$LIBDIR_PREFIX --with-screen=ncurses --with-x --with-ncurses-includes=${INST_PREFIX}/include --with-ncurses-libs=${LIBDIR_PREFIX} CPPFLAGS="-Wno-implicit-int -std=gnu99 -I${INST_PREFIX}/include" LDFLAGS="${COMPAT_IRIX_LIB} -lcvt -L${LIBDIR_PREFIX} -Wl,-rpath-link,${LIBDIR_PREFIX}" --cache-file=mc.cache
 
     make -j $MAKE_TASKS
 
@@ -446,6 +446,24 @@ if ! test -e make.installed; then
 
     popd
     touch make.installed
+fi
+
+GREP_VERSION=3.12
+if ! test -e grep.installed; then
+    download https://ftp.gnu.org/gnu/grep/grep-${GREP_VERSION}.tar.xz
+    tar xf grep-${GREP_VERSION}.tar.xz
+    pushd grep-${GREP_VERSION}
+    mkdir -p b
+    cd b
+
+    ../configure --prefix=$INST_PREFIX --host=${CROSS_PREFIX} --libdir=$LIBDIR_PREFIX --disable-year2038 --enable-threads=isoc CPPFLAGS="-std=gnu99 -Wno-implicit-function-declaration -I${INST_PREFIX}/include" LDFLAGS="${COMPAT_IRIX_LIB} -L${LIBDIR_PREFIX} -Wl,-rpath-link,${LIBDIR_PREFIX}"
+
+    make -j $MAKE_TASKS
+
+    make install
+
+    popd
+    touch grep.installed
 fi
 
 if ! test -e wolfssl.installed; then
@@ -514,6 +532,11 @@ EOF
     make -j $MAKE_TASKS
 
     make install
+
+    mkdir -p ${INST_PREFIX}/etc/init.d
+    cp -f ${TOPDIR}/misc/sshd ${INST_PREFIX}/etc/init.d/
+    chmod 755 ${INST_PREFIX}/etc/init.d/sshd
+    sed -ie 's/^#ListenAddress 0.0.0.0/ListenAddress 0.0.0.0/g' ${INST_PREFIX}/etc/ssh/sshd_config
 
     popd
     touch openssh.installed
